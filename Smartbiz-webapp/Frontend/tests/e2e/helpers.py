6885531abaed22c7230217fb7490d575f62deb99
SmartBiz <smartbiz@project.com>
2026-07-22 11:33:02 +0530

    fix: Build frontend and host on localhost:8080 in CI workflow to fix 404 test failures

--- Smartbiz-webapp/Frontend/tests/e2e/helpers.py
+++ Smartbiz-webapp/Frontend/tests/e2e/helpers.py
@@ -19,7 +19,7 @@ from selenium.webdriver.support import expected_conditions as EC
 # ---------------------------------------------------------------------------
 BASE_URL = os.environ.get(
     "SMARTBIZ_BASE_URL",
-    "https://thegitguy-56.github.io/Smartbiz-frontend/",
+    "http://localhost:8080/",
 )
 API_URL = os.environ.get(
     "SMARTBIZ_API_URL",
